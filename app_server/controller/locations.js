9eee217565cac1e94bdb26ee572026eb4cca9073
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-06 04:11:27 -0800

    changes made to use separate controller part from view

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,6 +1,32 @@
 module.exports.homeindex=function(req,res)
 {
-  res.render('list-location', { title: 'Home Page' });
+  res.render('list-location',{
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
 
 }
 module.exports.locationinformation=function(req,res)
