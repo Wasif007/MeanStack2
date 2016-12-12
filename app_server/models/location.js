261d23cf46f91202b9d150db290c04e1b2440bbd
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-12 09:58:40 -0800

    schema defining

---
+++ app_server/models/location.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+
+var openingHoursSchema=new mongoose.Schema({
+days:{type:String,required:true},
+opening:String,
+closing:String,
+closed:{type:Boolean,required:true}
+});
+
+var reviewsSchema=new mongoose.Schema({
+author:String,
+rating:{type:Number,"default":0,min:0,max:5,
+reviewText:String,
+timestamp:{type:Date,"default":Date.now()}}
+});
+
+var locationSchema=new mongoose.Schema({
+	name:{type:String,required:true},
+	address:String,
+	rating:{type:Number,"default":0,min:0,max:5},
+	facilites:[String],
+	coord:{type:[Number],index:"2dsphere"},
+	reviews:[reviewsSchema],
+	openingHours:[openingHoursSchema]
+
+});
