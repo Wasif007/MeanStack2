84731a96ccf6719edefbcf1e5c4ec045dcf0e02f
Wasif Ateeq <wasifateeq007@hotmail.com>
2016-12-14 04:51:44 -0800

    Post location document route made

--- getting-MEAN-chapter-06/app_api/controllers/locations.js
+++ getting-MEAN-chapter-06/app_api/controllers/locations.js
@@ -94,7 +94,27 @@ module.exports.locationsReadOne = function(req, res) {
 /* POST a new location */
 /* /api/locations */
 module.exports.locationsCreate = function(req, res) {
-
+  console.log(req.body);
+Loc.create({
+name:req.body.name,
+address:req.body.address,
+coords:[parseFloat(req.body.lng),parseFloat(req.body.lat)],
+openingTimes:[{
+  opening:req.body.opening1,
+  closing:req.body.closing1,
+  days:req.body.days1,
+  closed:req.body.closed1
+}],
+facilities:req.body.facilities.split(' , ')
+},function(err,locationCreated){
+  if(err){
+sendJSONresponse(res,404,err);
+return;
+  }
+  else{
+sendJSONresponse(res,200,locationCreated);
+  }
+});
 };
 
 /* PUT /api/locations/:locationid */
