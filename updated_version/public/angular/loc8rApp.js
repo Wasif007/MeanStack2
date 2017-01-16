86f3aad339a030cbd74267da2f070c0e6382f834
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-01-15 18:35:47 -0800

    locations changed according to local area

--- updated_version/public/angular/loc8rApp.js
+++ updated_version/public/angular/loc8rApp.js
@@ -4,19 +4,19 @@ angular.module('loc8rApp', []);
 var locationListCtrl = function ($scope) {
 $scope.data = {
 locations: [{
-name: 'Burger Queen',
-address: '125 High Street, Reading, RG6 1PS',
+name: 'Burger King',
+address: '125 High Street, Johar Town, Lahore',
 rating: 3,
 facilities: ['Hot drinks', 'Food', 'Premium wifi'],
 distance: '0.296456',
 _id: '5370a35f2536f6785f8dfb6a'
 },{
-name: 'Costy',
-address: '125 High Street, Reading, RG6 1PS',
+name: 'Hardees',
+address: '250 High Street, Garden Town, Karachi',
 rating: 5,
-facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+facilities: ['Hot drinks', 'Free Food', 'Alcoholic drinks'],
 distance: '0.7865456',
-_id: '5370a35f2536f6785f8dfb6a'
+_id: '5370a35f2536f6785f8dfb6b'
 }]};
 };
 
